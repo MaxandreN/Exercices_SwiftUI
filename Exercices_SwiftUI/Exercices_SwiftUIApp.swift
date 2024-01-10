589043d3048aa83b7aabc56d11339b8aaac3a6c0
Max <Maxandre.neveux@gmail.com>
2024-01-10 09:40:08 +0100

    Initial Commit

---
+++ Exercices_SwiftUI/Exercices_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercices_SwiftUIApp.swift
+//  Exercices_SwiftUI
+//
+//  Created by Maxandre Neveux on 10/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Exercices_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Exercices_SwiftUIUITests/Exercices_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Exercices_SwiftUIUITestsLaunchTests.swift
+//  Exercices_SwiftUIUITests
+//
+//  Created by Maxandre Neveux on 10/01/2024.
+//
+
+import XCTest
+
+final class Exercices_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
